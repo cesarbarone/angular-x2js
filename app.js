22e600e4df4f43c2acfd5686f0ee17c71281916f
César Barone <cesar.bmcosta@gmail.com>
2014-06-26 17:59:59 -0300

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+@toc
+1. setup - whitelist, appPath, html5Mode
+*/
+
+'use strict';
+
+angular.module('myApp', [
+'ngRoute', 'ngSanitize', 'ngTouch',		//additional angular modules
+'cesarbarone.angular-x2js'
+]).
+config(['$routeProvider', '$locationProvider', '$compileProvider', function($routeProvider, $locationProvider, $compileProvider) {
+	/**
+	setup - whitelist, appPath, html5Mode
+	@toc 1.
+	*/
+	$locationProvider.html5Mode(false);		//can't use this with github pages / if don't have access to the server
+	
+	// var staticPath ='/';
+	var staticPath;
+	// staticPath ='/angular-services/angular-x2js/';		//local
+	staticPath ='/';		//nodejs (local)
+	// staticPath ='/angular-x2js/';		//gh-pages
+	var appPathRoute ='/';
+	var pagesPath =staticPath+'pages/';
+	
+	
+	$routeProvider.when(appPathRoute+'home', {templateUrl: pagesPath+'home/home.html'});
+
+	$routeProvider.otherwise({redirectTo: appPathRoute+'home'});
+	
+}]);
---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'cbX2js', function($scope, cbX2js) {
+	//TODO - put any directive code here
+}]);
